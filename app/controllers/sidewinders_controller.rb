19f8a86a86b92d922232a3232d069c8b44ca5039
Ed Shuler <eshuler@mac.com>
2011-02-15 20:30:56 -0500

    More handsignals

--- app/controllers/sidewinders_controller.rb
+++ app/controllers/sidewinders_controller.rb
@@ -1,5 +1,9 @@
 class SidewindersController < ApplicationController
   
+  caches_page :media, :about, :organize, :links, :advance,
+  :flagging, :flag_surface, :flag_white, :flag_meatball, :flag_black, :flag_green, :flag_red, :flag_checkered,
+  :track_hpr, :track_pmp, :track_ppir, :track_lajunta
+  
   ############################
   # this is the default action
   ############################
@@ -18,7 +22,7 @@ class SidewindersController < ApplicationController
     @css = 'flags.css'
   end
   
-  # an action for the comm
+  # an action for communication
   def comm
     @title = 'Communication techniques'
     @css = 'comm.css'
@@ -71,129 +75,264 @@ class SidewindersController < ApplicationController
   # flags actions
   #################
   
-  def green
+  def flag_green
     @title = 'Green Flag'
     @css = 'flags.css'
   end
 
-  def yellow
+  def flag_yellow
     @title = 'Yellow Flag'
     @css = 'flags.css'
   end
 
-  def surface
+  def flag_surface
     @title = 'Surface Flag'
     @css = 'flags.css'
   end
 
-  def meatball
+  def flag_meatball
     @title = 'Mechanical Problem Flag'
     @css = 'flags.css'
   end
 
-  def passing
+  def flag_passing
     @title = 'Passing Flag'
     @css = 'flags.css'
   end
   
-  def behavior
+  def flag_black
     @title = 'Bad Behavior Flag'
     @css = 'flags.css'
   end
   
-  def red
+  def flag_red
     @title = 'Stop NOW Flag'
     @css = 'flags.css'
   end
   
-  def white
+  def flag_white
     @title = 'Slow vehicle Flag'
     @css = 'flags.css'
   end
   
-  def checkered
+  def flag_checkered
     @title = 'Session Ended Flag'
     @css = 'flags.css'
   end
   
-  ###################
-  # coverage actions
-  ###################
-  def hpr
+  #########################
+  # track coverage actions
+  #########################
+  def track_hpr
     @title = 'High Plains Raceway'
     @css = 'coverage.css'
   end
   
-  def ppir
+  def track_ppir
     @title = 'High Plains Raceway'
     @css = 'coverage.css'
   end
   
-  def lajunta
+  def track_lajunta
     @title = 'High Plains Raceway'
     @css = 'coverage.css'
   end
   
-  def pmp
+  def track_pmp
     @title = 'High Plains Raceway'
     @css = 'coverage.css'
   end
   
-  ###################
-  # signal actions
-  ###################
+  ######################
+  # hand signal actions
+  ######################
   def sig_safetycomm
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
   
+  def sig_sc_ambulance
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_sc_flat_tow
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_sc_tilt_bed
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_sc_send_help
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_sc_send_now
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_sc_send_lots_of_help
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_sc_fire
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_sc_wrecker
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  #------------------------
   def sig_captflagger
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
   
-  def sig_standing_yellow
+  def sig_cf_standing_yellow
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
     
-  def sig_waving_yellow
+  def sig_cf_waving_yellow
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
     
-  def sig_dbl_standing_yellow
+  def sig_cf_dbl_standing_yellow
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
     
-  def sig_surface
+  def sig_cf_surface
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
     
-  def sig_white
+  def sig_cf_white
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
-    
+  #------------------------
   def sig_numbers
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
-    
+  
+  def sig_num_one
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_two
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_three
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_four
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_five
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_six
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_seven
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_eight
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_nine
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_zero
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_num_request
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  #-------------------------
   def sig_colors
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
-    
+  
+  def sig_color_red
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_color_white
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_color_brown
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_color_yellow
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_color_blue
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_color_black
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  def sig_color_green
+    @title = 'Hand Signals'
+    @css = 'signals.css'
+  end
+  
+  
+  #--------------------------
   def sig_general
     @title = 'Hand Signals'
     @css = 'signals.css'
   end
   
-  def images
-  end
+  #########################
+  # other actions
+  #########################
   
   def weather
     @title = 'Weather'
