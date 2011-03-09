bee314652fff8487cd1b93185a1c2f7baaf88f73
Ed Shuler <eshuler@mac.com>
2011-03-09 13:02:07 -0700

    finished audio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => '0e9a3f159446e9310a96978d5bf7739c'
+  # protect_from_forgery :secret => '0e9a3f159446e9310a96978d5bf7739c'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
